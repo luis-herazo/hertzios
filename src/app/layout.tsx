d31f8b512133d256831bc76d17a2ca56dfcd02ad
Luis Herazo <h2toxic@gmail.com>
2026-02-06 10:55:13 -0400

    fix: Add `suppressHydrationWarning` prop to the `<html>` tag.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-background font-sans text-foreground`}
       >
